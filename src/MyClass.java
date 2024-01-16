d2826a32ab94c86a3007dfb16603b62f8a0e2839
unknown <filipdjurdjevac12356@gmail.com>
2024-01-16 11:02:37 +0100

    first commit

---
+++ src/MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass {
+
+public static void main(String[] args) {
+MyClass2 v1 = new MyClass2(); // Using default color "Blue"
+MyClass2 v2 = new MyClass2("Red"); // Setting color to "Red"
+System.out.println("Vehicle color: " + v1); // Prints "Vehicle color: Blue"
+System.out.println("Vehicle color: " + v2); // Prints "Vehicle color: Red"
+}
+}
---
+++ src/MyClass2.java
@@ -0,0 +1,13 @@
+public class MyClass2 {
+private String color;
+// Constructor with a default color
+MyClass2() {
+color = "Blue";
+}
+MyClass2(String a) {
+color = a;
+}
+public String toString() {
+return color;
+}
+}
